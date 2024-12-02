07ab151d0f14a38881b6937d7d32597911694ad1
ixs <ichimaru.vistar.smile@gmail.com>
2024-12-03 00:57:55 +0800

    fix: 暂时屏蔽了字体资源的加载, 防止资源加载失败, 应用无法正常启动的问题

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,17 @@
-import { Inter, Lusitana } from 'next/font/google';
+// 字体会加载失败导致应用无法正常启动
+// import { Inter, Lusitana } from 'next/font/google';
 
-// Body主字体，指定字体子集为拉丁文
-export const inter = Inter({ subsets: ['latin'] });
+// // Body主字体，指定字体子集为拉丁文
+// export const inter = Inter({ subsets: ['latin'] });
 
-// 添加辅助字体
-export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' })
+// // 添加辅助字体
+// export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' })
+
+export const inter = {
+  className: 'Inter'
+}
+
+export const lusitana = {
+  className: 'Lusitana'
+}
 
